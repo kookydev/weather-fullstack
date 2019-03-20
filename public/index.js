583638d2f796c570a63df6d84196e2a6d35997ad
Kyle <kwjtdev@gmail.com>
2019-03-20 00:29:17 +0000

    More Progress Made, Icons to do

--- public/index.js
+++ public/index.js
@@ -2,8 +2,12 @@ console.log("client javascript initiated")
 const inputText = document.getElementById("input-text")
 const submitButton = document.getElementById("submit-button")
 let bodyStyle = document.body.style
-
-
+const loading = document.getElementById("loading")
+const search = document.getElementById("search")
+const logo = document.getElementById("logo")
+const result = document.getElementById("result")
+const placename = document.getElementById("placename")
+const summary = document.getElementById("summary")
 
 const getWindowSize = () => {
     const doc = document,
@@ -22,22 +26,43 @@ const getWindowSize = () => {
     };
 }
 
+let returnedData = {};
+let picPreload =
 
-submitButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    const winSize = getWindowSize()
-    const orientation = winSize.height > winSize.width ? "portrait" : "landscape"
-    if (inputText.value) {
-        fetch(`/weather?address=${inputText.value}&w=${winSize.width}&h=${winSize.height}&orientation=${orientation}`).then((response) => {
-            response.json().then((data) => {
-                console.log(data)
-                bodyStyle.backgroundImage = `url("${data.img.imgURL}")`
+    submitButton.addEventListener("click", (e) => {
+        e.preventDefault()
+        const winSize = getWindowSize()
+        const orientation = winSize.height > winSize.width ? "portrait" : "landscape"
+        if (inputText.value) {
+            loading.classList = "loading show"
+            search.classList = "search"
+
+            fetch(`/weather?address=${inputText.value}&w=${winSize.width}&h=${winSize.height}&orientation=${orientation}`).then((response) => {
+                response.json().then((data) => {
+                    console.log(data)
+
+                    // var src = `url("${}")`
+                    var url = data.img.imgURL
+                    var img = new Image();
+                    img.src = url;
+                    img.onload = function () {
+                        bodyStyle.backgroundImage = `url("${data.img.imgURL}")`
+                        loading.classList = "loading"
+                        search.classList = "search show"
+                        result.classList = "result show"
+                        placename.innerText = data.location
+                        summary.innerText = data.forecast.summary
+
+                    }
+
+
+
+                })
             })
-        })
 
-    } else {
-        console.log("That didn't work!!!")
-    }
+        } else {
+            console.log("That didn't work!!!")
+        }
 
 
-})
+    })
